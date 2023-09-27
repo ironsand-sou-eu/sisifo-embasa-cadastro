f9102c715f162509c8406e5da97b568fe59405a0
César Rodriguez <54708668+ironsand-sou-eu@users.noreply.github.com>
2023-09-27 02:41:42 +0200

    Bugfixed EspaiderProcessoDataStructure

--- src/js/data-structures/EspaiderProcessoDataStructure.js
+++ src/js/data-structures/EspaiderProcessoDataStructure.js
@@ -4,7 +4,7 @@ class EspaiderProcessoDataStructure
 {
     constructor({ numeroProcesso, nomeAdverso, cpfCnpjAdverso, tipoAdverso, condicaoAdverso, advogadoAdverso,
         valorCausa, natureza, tipoAcao, causaPedir, orgao, juizo, comarca, rito, nucleo, advogado,
-        prepostoNucleo, unidade, divisao, responsavelRegressivo, dataCitacao, gerencia, sistema, errorMsgs = [],
+        preposto, unidade, divisao, responsavelRegressivo, dataCitacao, gerencia, sistema, errorMsgs = [],
         peticionamento = hardcoded.peticionamento, nomeEmbasa = hardcoded.nomeEmbasa,
         cnpjEmbasa = hardcoded.cnpjEmbasa, tipoEmpresaEmbasa = hardcoded.tipoEmpresaEmbasa,
         condicaoEmpresaEmbasa = hardcoded.condicaoEmpresaEmbasa, nomeDesdobramento = hardcoded.nomeDesdobramento,
@@ -13,7 +13,7 @@ class EspaiderProcessoDataStructure
         Object.assign(this, {
             numeroProcesso, nomeAdverso, cpfCnpjAdverso, tipoAdverso, condicaoAdverso,
             advogadoAdverso, valorCausa, natureza, classe, tipoAcao, causaPedir, orgao,
-            juizo, comarca, rito, fase, nucleo, advogado, prepostoNucleo, unidade,
+            juizo, comarca, rito, fase, nucleo, advogado, preposto, unidade,
             divisao, nomeDesdobramento, responsavelRegressivo, peticionamento, nomeEmbasa,
             cnpjEmbasa, tipoEmpresaEmbasa, condicaoEmpresaEmbasa, gerencia, sistema, errorMsgs,
             dataCitacao: dataCitacao ? new Date(dataCitacao.getTime()) : undefined
