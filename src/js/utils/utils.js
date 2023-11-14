aa4307022c6e39144259da731ce7e32342084172
César Rodriguez <54708668+ironsand-sou-eu@users.noreply.github.com>
2023-11-14 23:45:34 +0100

    Added localStorage getter function and prettified

--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -1,70 +1,102 @@
-import { nomesEmbasa, operators } from "../enums"
+import { nomesEmbasa, operators } from "../enums";
 
-export const REGEX_CNJ_NUMBER = /(\d{7}-\d{2}.\d{4}.)(\d)(.\d{2}.\d{4})/
+export const REGEX_CNJ_NUMBER = /(\d{7}-\d{2}.\d{4}.)(\d)(.\d{2}.\d{4})/;
 
 export default function compareWithOperator(a, operator, b) {
-    if (a === undefined || b === undefined) return false
-    switch (operator) {
+  if (a === undefined || b === undefined) return false;
+  switch (operator) {
     case operators.sensitiveStrictEquality:
-        return a === b
+      return a === b;
     case operators.insensitiveStrictEquality:
-        return a.toString().toLowerCase() === b.toString().toLowerCase()
+      return a.toString().toLowerCase() === b.toString().toLowerCase();
     case operators.insentiviveIncludes:
-        return a.toLowerCase().includes(b.toLowerCase())
+      return a.toLowerCase().includes(b.toLowerCase());
     case operators.includes:
-        return a.includes(b)
+      return a.includes(b);
     case operators.numericEquality:
-        return Number(a) === Number(b)
-    }
+      return Number(a) === Number(b);
+  }
 }
 
 export function debounce(cb, delay = 250) {
-    let timeOut
-    return (...args) => {
-        clearTimeout(timeOut)
-        timeOut = setTimeout(() => {
-            cb(...args)
-        }, delay)
-    }
+  let timeOut;
+  return (...args) => {
+    clearTimeout(timeOut);
+    timeOut = setTimeout(() => {
+      cb(...args);
+    }, delay);
+  };
 }
 
 export function toISOStringInSalvadorTimezone(date, outputHours = false) {
-    return toBrDateString(date, outputHours, true)
+  return toBrDateString(date, outputHours, true);
 }
 
 export function toBrDateString(date, outputHours = false, isoFormat = false) {
-    if (!date) return null
-    const dateObj = new Date(date)
-    const dateCompensatingTimezone = new Date(dateObj.getTime() - (3 * 60 * 60 * 1000))
-    const year = dateCompensatingTimezone.getUTCFullYear()
-    const month = String(dateCompensatingTimezone.getUTCMonth() + 1).padStart(2, "0")
-    const day = String(dateCompensatingTimezone.getUTCDate()).padStart(2, "0")
-    let finalStr = isoFormat ? `${year}-${month}-${day}` : `${day}/${month}/${year}`
-    if (outputHours) {
-        const hour = String(dateCompensatingTimezone.getUTCHours()).padStart(2, "0")
-        const minute = String(dateCompensatingTimezone.getUTCMinutes()).padStart(2, "0")
-        const dateToHourSeparator = isoFormat ? "T" : " "
-        finalStr += `${dateToHourSeparator}${hour}:${minute}`
-    }
-    return finalStr
+  if (!date) return null;
+  const dateObj = new Date(date);
+  const dateCompensatingTimezone = new Date(
+    dateObj.getTime() - 3 * 60 * 60 * 1000
+  );
+  const year = dateCompensatingTimezone.getUTCFullYear();
+  const month = String(dateCompensatingTimezone.getUTCMonth() + 1).padStart(
+    2,
+    "0"
+  );
+  const day = String(dateCompensatingTimezone.getUTCDate()).padStart(2, "0");
+  let finalStr = isoFormat
+    ? `${year}-${month}-${day}`
+    : `${day}/${month}/${year}`;
+  if (outputHours) {
+    const hour = String(dateCompensatingTimezone.getUTCHours()).padStart(
+      2,
+      "0"
+    );
+    const minute = String(dateCompensatingTimezone.getUTCMinutes()).padStart(
+      2,
+      "0"
+    );
+    const dateToHourSeparator = isoFormat ? "T" : " ";
+    finalStr += `${dateToHourSeparator}${hour}:${minute}`;
+  }
+  return finalStr;
 }
 
 export function lastWorkdayUntil(date) {
-    let weekendDaysToSubtract = 0
-    if (date.getDay() === 0) weekendDaysToSubtract = -2
-    if (date.getDay() === 6) weekendDaysToSubtract = -1
-    //TODO: compute holidays
-    return addDaysToDate(date, weekendDaysToSubtract)
+  let weekendDaysToSubtract = 0;
+  if (date.getDay() === 0) weekendDaysToSubtract = -2;
+  if (date.getDay() === 6) weekendDaysToSubtract = -1;
+  //TODO: compute holidays
+  return addDaysToDate(date, weekendDaysToSubtract);
 }
 
 export function addDaysToDate(date, daysToAdd) {
-    return new Date(date.getTime() + parseInt(daysToAdd) * 24 * 60 * 60 * 1000 )
+  return new Date(date.getTime() + parseInt(daysToAdd) * 24 * 60 * 60 * 1000);
 }
 
 export function isNumber(x) {
-    return parseFloat(x) == x
+  return parseFloat(x) == x;
 }
 
 export function parteEhEmbasa(nome) {
-    return nomesEmbasa.some(nomeEmbasa => nome.toLowerCase().includes(nomeEmbasa.toLowerCase()))
+  return nomesEmbasa.some(nomeEmbasa =>
+    nome.toLowerCase().includes(nomeEmbasa.toLowerCase())
+  );
+}
+
+export function getOrCreateTodaysCurrentSheetInLocalStorage() {
+  const currentSheet = JSON.parse(localStorage.getItem("currentSheet"));
+  if (
+    currentSheet &&
+    new Date(currentSheet.date).toLocaleDateString("pt-BR") ===
+      new Date().toLocaleDateString("pt-BR")
+  ) {
+    return currentSheet;
+  }
+  const newSheet = {
+    date: new Date().toISOString().substring(0, 10),
+    number: "01",
+  };
+  localStorage.setItem("currentSheet", JSON.stringify(newSheet));
+  return newSheet;
 }
